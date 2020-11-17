2b022c3c0d7f921275d58fa771420a00684fc936
yanix <smirnovskoe@gmail.com>
2020-11-17 15:43:12 +0300

    added form for task

--- tasks_app/views.py
+++ tasks_app/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
     tasks = Task.objects.all()
 
-    context = {'tasks': tasks}
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks_app/list.html', context)
