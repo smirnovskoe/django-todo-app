02b8302caa6bcae87e5159ae199a0a119282d985
yanix <smirnovskoe@gmail.com>
2020-11-17 15:25:59 +0300

    added simple template for task

--- core/settings.py
+++ core/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEMPLATES_DIR = Path(__file__).resolve().parent.parent.joinpath('templates')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'tasks_app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- tasks_app/views.py
+++ tasks_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+
+def index(request):
+    tasks = Task.objects.all()
+
+    context = {'tasks': tasks}
+    return render(request, 'tasks_app/list.html', context)
