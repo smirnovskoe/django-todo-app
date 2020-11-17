e385f82af45249c3a62579d0ae481f20b4edb596
yanix <smirnovskoe@gmail.com>
2020-11-17 16:34:24 +0300

    added update task form

--- tasks_app/views.py
+++ tasks_app/views.py
@@ -17,3 +17,19 @@ def index(request):
 
     context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks_app/list.html', context)
+
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid:
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+
+    return render(request, 'tasks_app/update_task.html', context)
